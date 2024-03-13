b3e8a561f0a3525f5481d9ae30d5346c6bae0eb5
Gabriel-Rocha1 <gabrielrochasant@gmail.com>
2024-03-13 19:23:40 -0300

    Project setup

---
+++ src/main/java/br/ufrn/imd/interfaces/Server.java
@@ -0,0 +1,7 @@
+package br.ufrn.imd.interfaces;
+
+public interface Server {
+    // Create methods to abstract the communication between client and server
+    // Implement this interface with Sockets and RPC/RMI
+    // Consume this interface on the client-side
+}
---
+++ src/main/java/br/ufrn/imd/server/SocketServer.java
@@ -0,0 +1,12 @@
+package br.ufrn.imd.server;
+
+import br.ufrn.imd.interfaces.Server;
+
+public class SocketServer implements Server {
+    // start the bot
+    // set up the server socket
+    // listen for connections
+    // wait for messages
+    // run them through the bot
+    // send reply
+}
