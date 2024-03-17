5442d2559d44878583daf0f55b3afc463ea2069f
gabriel-rch <gabrielrochasant@gmail.com>
2024-03-17 10:15:11 -0300

    feat: add RMI client-server communication

---
+++ src/main/java/br/ufrn/imd/server/RMIServer.java
@@ -0,0 +1,35 @@
+package br.ufrn.imd.server;
+
+import br.ufrn.imd.interfaces.Chat;
+
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class RMIServer implements Chat {
+    private final Chatbot bot;
+
+    public RMIServer() {
+        bot = new Chatbot();
+    }
+
+    @Override
+    public String ask(String question) throws RemoteException {
+        return bot.answer(question);
+    }
+
+    public static void main(String[] args) {
+        try {
+            var server = new RMIServer();
+            var stub = (Chat) UnicastRemoteObject.exportObject(server, 0);
+
+            var registry = LocateRegistry.createRegistry(1337);
+            registry.bind("Chat", stub);
+
+            System.out.println("Server ready!");
+        } catch (AlreadyBoundException | RemoteException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
